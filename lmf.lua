b3ce99a994c2d0d1d1996a7aa0e1aaa8105a0305
kiccer <1072907338@qq.com>
2020-01-09 19:01:42 +0800

    update 1. 从 kiccer/Soldier76 中提取工具函数

---
+++ lmf.lua
@@ -0,0 +1,158 @@
+--------------------------------------- User config settings ---------------------------------------
+
+userConfig = {
+
+}
+
+----------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------- Framework built-in code -------------------------------------
+lmf = {
+
+}
+
+--[[ tools ]]
+
+-- split function
+function string.split (str, s)
+	if string.find(str, s) == nil then return { str } end
+
+	local res = {}
+	local reg = "(.-)" .. s .. "()"
+	local index = 0
+	local last_i
+
+	for n, i in string.gfind(str, reg) do
+		index = index + 1
+		res[index] = n
+		last_i = i
+	end
+
+	res[index + 1] = string.sub(str, last_i)
+
+	return res
+end
+
+-- Javascript Array.prototype.reduce
+function table.reduce (t, c)
+	local res = c(t[1], t[2])
+	for i = 3, #t do res = c(res, t[i]) end
+	return res
+end
+
+-- Javascript Array.prototype.map
+function table.map (t, c)
+	local res = {}
+	for i = 1, #t do res[i] = c(t[i], i) end
+	return res
+end
+
+-- Javascript Array.prototype.forEach
+function table.forEach (t, c)
+	for i = 1, #t do c(t[i], i) end
+end
+
+--[[
+	* 打印 table
+	* @param  {any} val     传入值
+	* @return {str}         格式化后的文本
+]]
+function table.print (val)
+	if type(val) == "nil" then return type(val) end
+
+	local function loop (val, keyType, _indent)
+		if not val then return end
+		_indent = _indent or 1
+		keyType = keyType or "string"
+		local res = ""
+		local indentStr = "     " -- 缩进空格
+		local indent = string.rep(indentStr, _indent)
+		local end_indent = string.rep(indentStr, _indent - 1)
+		local putline = function (...)
+			local arr = { res, ... }
+			for i = 1, #arr do arr[i] = tostring(arr[i]) end
+			res = table.concat(arr)
+		end
+
+		if type(val) == "table" then
+			putline("{ ")
+
+			if #val > 0 then
+				local index = 0
+				for k, v in pairs(val) do
+					index = index + 1
+					if type(v) == "table" then
+						if index == 1 then putline("\n") end
+						putline(indent, loop(v, type(k), _indent + 1), "\n")
+						if index == #val then putline(end_indent) end
+					else
+						putline(loop(v, type(k), _indent + 1))
+					end
+				end
+			else
+				putline("\n")
+				for k, v in pairs(val) do
+					putline(indent, k, " = ", loop(v, type(k), _indent + 1), "\n")
+				end
+				putline(end_indent)
+			end
+
+			putline("}, ")
+		elseif type(val) == "string" then
+			val = string.gsub(val, "\a", "\\a") -- 响铃(BEL)
+			val = string.gsub(val, "\b", "\\b") -- 退格(BS),将当前位置移到前一列
+			val = string.gsub(val, "\f", "\\f") -- 换页(FF),将当前位置移到下页开头
+			val = string.gsub(val, "\n", "\\n") -- 换行(LF),将当前位置移到下一行开头
+			val = string.gsub(val, "\r", "\\r") -- 回车(CR),将当前位置移到本行开头
+			val = string.gsub(val, "\t", "\\t") -- 水平指标(HT),(调用下一个TAB位置)
+			val = string.gsub(val, "\v", "\\v") -- 垂直指标(VT)
+			putline("\"", val, "\", ")
+		elseif type(val) == "boolean" then
+			putline((val and {"true"} or {"false"})[1], ", ")
+		elseif type(val) == "function" then
+			putline(tostring(val), ", ")
+		elseif type(val) == "nil" then
+			putline("nil, ")
+		else
+			putline(val, ", ")
+		end
+
+		return res
+	end
+
+	local res = loop(val)
+	res = string.gsub(res, ",(%s*})", "%1")
+	res = string.gsub(res, ",(%s*)$", "%1")
+	res = string.gsub(res, "{%s+}", "{}")
+
+	return res
+end
+
+-- console
+console = {}
+function console.log (str)
+	OutputLogMessage(table.print(str) .. "\n")
+end
+
+--[[ 入口方法 ]]
+function OnEvent (event, arg, family)
+	console.log("event = " .. event .. ", arg = " .. arg .. ", family = " .. family)
+end
+----------------------------------------------------------------------------------------------------
